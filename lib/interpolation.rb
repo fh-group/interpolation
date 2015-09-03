3b3c61c22fd671ebd297347065eafdbc21fb3087
Alexander Ambrose <alexa@fh-group.com>
2015-09-03 11:19:17 -0400

    Removed nmatrix dependency

--- lib/interpolation.rb
+++ lib/interpolation.rb
@@ -1,17 +1,15 @@
 #--
-# = Interpolation 
+# = Interpolation
 # Copyright (c) 2014, Sameer Deshmukh
 # All rights reserved.
-#   
-# Interpolation is a library for executing various interpolation 
+#
+# Interpolation is a library for executing various interpolation
 # functions in Ruby.
-# 
+#
 # == spec/lib/1d_interpolation_spec.rb
 #
 # Tests for 1D interpolation.
 #++
-
-require 'nmatrix'
 
 $:.unshift File.dirname(__FILE__)
 
